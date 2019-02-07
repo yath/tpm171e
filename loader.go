1dd10b6e5ae74b3b8815d473bec79babceda4537
Sebastian Schmidt <yath@yath.de>
2019-02-07 23:49:54 +0100

    Acquire a thread dump for determining module base.
    This is still lacking the load of dtv_driver.ko and correlating it
    with the thread dump’s symbols.

--- loader.go
+++ loader.go
@@ -1,24 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"debug/elf"
 	"encoding/binary"
+	"encoding/gob"
 	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"sync"
+	"syscall"
+	"unsafe"
 )
 
 var flagAsset = flag.String("asset", "findsym.elf", "name of the bundled asset")
-var flagLoadAddr = flag.Uint("load_addr", 0xbf0cde8c, "load address (i.e. _CmdVersion)")
+var flagLoadAddr = flag.Uint("load_addr", 0, "load address (i.e. _CmdVersion)")
 var flagDump = flag.String("dump", "", "dump final relocated binary to given filename")
+
+var warning = log.New(os.Stderr, "WARNING: ", log.LstdFlags)
 
 // from debug/elf/file.go.
 func applyRelocationsARM(f *elf.File, symbols []elf.Symbol, dst []byte, rels []byte, daddr addr) error {
-	// 8 is the size of Rel32.
+	// 8 is the size of elf.Rel32.
 	if len(rels)%8 != 0 {
 		return errors.New("length of relocation section is not a multiple of 8")
 	}
@@ -136,6 +146,227 @@ func loadELF(data []byte, loadaddr addr) ([]byte, error) {
 	return ret, nil
 }
 
+func sendString(f *os.File, cmd string) error {
+	bp, err := syscall.BytePtrFromString(cmd)
+	if err != nil {
+		return fmt.Errorf("can't get a byte buffer: %v", err)
+	}
+
+	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(f.Fd()), 1, uintptr(unsafe.Pointer(bp)))
+	if errno != 0 {
+		return fmt.Errorf("ioctl(%v, INPUT_STRING, %q) failed: %v", f, cmd, errno)
+	}
+
+	return nil
+}
+
+func ignoreSIGKILL(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	if ee, ok := err.(*exec.ExitError); ok {
+		if ws, ok := ee.ProcessState.Sys().(syscall.WaitStatus); ok {
+			if ws.Signaled() && ws.Signal() == os.Kill {
+				return nil
+			}
+		}
+	}
+	return err
+}
+
+func cliCommand(f *os.File, cmdstr string, recv chan<- string) error {
+	cmd := exec.Command("logcat", "-T0", "-s", "MTK_KL")
+	cmd.Stdin = nil
+	cmd.Stderr = nil
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("can't get a pipe to logcat's stdout: %v", err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("can't start logcat: %v", err)
+	}
+
+	defer func() {
+		cmd.Process.Kill()
+		if err := ignoreSIGKILL(cmd.Wait()); err != nil {
+			warning.Printf("When waiting for logcat: %v", err)
+		}
+	}()
+
+	s := bufio.NewScanner(stdout)
+	if !s.Scan() {
+		ret := s.Err()
+		if ret == nil {
+			ret = errors.New("unexpected EOF while waiting for first line")
+		}
+		return ret
+	}
+
+	go func() {
+		for s.Scan() {
+			line := s.Text()
+			if len(line) > 0 && line[0] == '-' {
+				continue
+			}
+			recv <- line
+		}
+		close(recv)
+		if err := s.Err(); err != nil {
+			warning.Printf("While reading from logcat: %v", err)
+		}
+	}()
+
+	// Will block until the command has been processed. The log does lag a bit, but
+	// it doesn’t matter if we lose a couple of lines at the end of a thread dump.
+	return sendString(f, cmdstr)
+
+	// defer kills logcat, goroutine closes recv on EOF.
+}
+
+func mustAtoi(s string) uint32 {
+	v, err := strconv.ParseUint(s, 16, 32)
+	if err != nil {
+		log.Panicf("ParseUint(%q, 16, 32) unexpectedly failed: %v", s, err)
+	}
+	return uint32(v)
+}
+
+var symRE = regexp.MustCompile(`\[<([[:xdigit:]]{8})>\] \((.*?)\+0x([[:xdigit:]]+)/`)
+
+func getThreadDump() (map[string]addr, error) {
+	f, err := os.Open("/dev/cli")
+	if err != nil {
+		return nil, fmt.Errorf("can't open CLI: %v", err)
+	}
+	defer f.Close()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	recv := make(chan string)
+	go func() {
+		err = cliCommand(f, "b.da", recv)
+		wg.Done()
+	}()
+
+	inconclusive := make(map[string]struct{})
+	ret := make(map[string]addr)
+	for line := range recv {
+		for _, m := range symRE.FindAllStringSubmatch(line, -1) {
+			abs, sym, off := mustAtoi(m[1]), m[2], mustAtoi(m[3])
+			symaddr := addr(abs - off)
+			if old, ok := ret[sym]; ok && old != symaddr {
+				if _, iok := inconclusive[sym]; !iok {
+					warning.Printf("Inconclusive address for %q: %08x (previous) vs. %08x (current). Ignoring symbol.", sym, old, symaddr)
+					inconclusive[sym] = struct{}{}
+				}
+			} else if !ok {
+				log.Printf("Discovered %q at 0x%08x", sym, symaddr)
+				ret[sym] = symaddr
+			}
+		}
+	}
+	wg.Wait()
+
+	if err != nil {
+		return nil, fmt.Errorf("can't send CLI command: %v", err)
+	}
+
+	for sym, _ := range inconclusive {
+		delete(ret, sym)
+	}
+
+	return ret, nil
+}
+
+type cache struct {
+	KernelVersion string
+	TargetSymbol  string
+	TargetAddr    addr
+}
+
+const cacheFile = "/data/local/tmp/targetcache"
+
+func getCache() *cache {
+	f, err := os.Open(cacheFile)
+	if err != nil {
+		warning.Printf("Can't open cache file, ignoring: %v", err)
+		return nil
+	}
+	defer f.Close()
+
+	var ret cache
+	if err := gob.NewDecoder(f).Decode(&ret); err != nil {
+		warning.Printf("Can't decode %v as %T, ignoring: %v", cacheFile, &ret, err)
+		return nil
+	}
+
+	return &ret
+}
+
+func putCache(c *cache) error {
+	f, err := os.Create(cacheFile)
+	if err != nil {
+		return fmt.Errorf("can't create cache file: %v", err)
+	}
+
+	if err := gob.NewEncoder(f).Encode(c); err != nil {
+		os.Remove(cacheFile)
+		return fmt.Errorf("can't encode %T%#v to %v: %v", c, c, f, err)
+	}
+
+	if err := f.Close(); err != nil {
+		os.Remove(cacheFile)
+		return fmt.Errorf("can't close cache file: %v", err)
+	}
+
+	return nil
+}
+
+func getKernelVersion() (string, error) {
+	v, err := ioutil.ReadFile("/proc/version")
+	return string(v), err
+}
+
+const targetSymbol = "CLI_CmdList" // XXX
+
+func getTargetAddr() (addr, error) {
+	// XXX: correlate with dtv_mod.ko's symbols
+	kver, err := getKernelVersion()
+	if err != nil {
+		return addr(0), fmt.Errorf("can't determine running kernel: %v", err)
+	}
+
+	if c := getCache(); c != nil {
+		if c.KernelVersion != kver {
+			warning.Printf("Cached kernel version (%q) doesn't match running kernel (%q)", c.KernelVersion, kver)
+		} else if c.TargetSymbol != targetSymbol {
+			warning.Printf("Cache symbol name (%q) doesn't match expected (%q)", c.TargetSymbol, targetSymbol)
+		} else {
+			log.Printf("Loaded target symbol %q = 0x%08x from cache, delete %q if this is incorrect", targetSymbol, c.TargetAddr, cacheFile)
+			return c.TargetAddr, nil
+		}
+	}
+
+	warning.Printf("Need to acquire a thread dump, this might take a minute or two.")
+	runsyms, err := getThreadDump()
+	if err != nil {
+		return addr(0), fmt.Errorf("can't acquire thread dump: %v", err)
+	}
+
+	a, ok := runsyms[targetSymbol]
+	if !ok {
+		return addr(0), fmt.Errorf("symbol %q not found (in %#v)", targetSymbol, runsyms)
+	}
+
+	if err := putCache(&cache{KernelVersion: kver, TargetSymbol: targetSymbol, TargetAddr: a}); err != nil {
+		warning.Printf("Can't store cache: %v", err)
+	}
+
+	return a, nil
+}
+
 func main() {
 	flag.Parse()
 
@@ -144,7 +375,16 @@ func main() {
 		log.Fatalf("Can't load asset %q: %v", *flagAsset, err)
 	}
 
-	data, err := loadELF(a, addr(*flagLoadAddr))
+	loadAddr := addr(*flagLoadAddr)
+	if loadAddr == addr(0) {
+		var err error
+		loadAddr, err = getTargetAddr()
+		if err != nil {
+			log.Fatalf("Load address was not specified but can't determine target address: %v", err)
+		}
+	}
+
+	data, err := loadELF(a, loadAddr)
 	if err != nil {
 		log.Fatalf("Can't load ELF: %v", err)
 	}
@@ -154,12 +394,12 @@ func main() {
 			log.Fatalf("Can't dump data: %v", err)
 		}
 		log.Printf("Output written to %q", *flagDump)
-		log.Printf("arm-none-eabi-objdump -marm -bbinary -EL -D --adjust-vma=0x%08x %s", *flagLoadAddr, *flagDump)
+		log.Printf("arm-none-eabi-objdump -marm -bbinary -EL -D --adjust-vma=0x%08x %s", loadAddr, *flagDump)
 		os.Exit(0)
 	}
 
 	for i := 0; i < len(data); i += 4 {
-		cmd := fmt.Sprintf("w 0x%08x 0x%02x%02x%02x%02x", int(*flagLoadAddr)+i, data[i+3], data[i+2], data[i+1], data[i])
+		cmd := fmt.Sprintf("w 0x%08x 0x%02x%02x%02x%02x", uint(loadAddr)+uint(i), data[i+3], data[i+2], data[i+1], data[i])
 		fmt.Printf("adb shell cli_shell %s < /dev/null\n", cmd)
 	}
 }
